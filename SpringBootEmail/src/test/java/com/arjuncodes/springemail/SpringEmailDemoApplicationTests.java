faf93443017fea809aa6a282f160ab59ac99aaf4
aniketkedar <aniketkedar4@gmail.com>
2023-03-27 20:54:09 +0530

    Add files via upload

---
+++ SpringBootEmail/src/test/java/com/arjuncodes/springemail/SpringEmailDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arjuncodes.springemail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEmailDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
